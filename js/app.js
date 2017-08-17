fc999a98b7ae489910dbdc8afdc8162271d1f984
sauravkh <skharb@uw.edu>
2017-08-17 18:43:36 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+ 
+
+ var typed3 = new Typed('#typed3', {
+    strings: ['I am a data programmer', 'I am a web developer', 'I am a co-founder', 'I am a machine learning enthusiast','and love Hiking and kayaking'],
+    typeSpeed: 40,
+    backSpeed: 20,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+
+  var typed2 = new Typed('#typed2', {
+    strings: ['Saurav Kharb'],
+    typeSpeed: 30,
+    backSpeed: 0,
+    cursorChar:'',
+    fadeOut: true,
+    loop: false
+  });
+
+
+
